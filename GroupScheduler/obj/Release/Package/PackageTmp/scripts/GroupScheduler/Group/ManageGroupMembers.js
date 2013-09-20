e9e0f46532f930c2813b6c1496d869eb4674fad7
AndrewJSeverson <severa24@uwosh.edu>
2013-09-19 19:39:06 -0500

    Initial Commit

---
+++ GroupScheduler/obj/Release/Package/PackageTmp/scripts/GroupScheduler/Group/ManageGroupMembers.js
@@ -0,0 +1,83 @@
+﻿$(document).ready(function() {
+    ManageGroupMembers.Init();
+});
+
+var ManageGroupMembers = new function () {
+    // the current row being edited
+    var currentRow = null;
+    this.Init = function() {
+        // button click event for updating a member to an admin
+        $(".DemoteAdmin, .PromoteMember").click(function () {
+            currentRow = $(this).parent("td").parent("tr");
+            updateMemberRole();
+            $(this).removeClass("btn-danger").removeClass("btn-success");
+
+        });
+
+        // button click to remove a member from a group
+        $(".RemoveMember").click(function() {
+            currentRow = $(this).parent("td").parent("tr");
+            removeMember();
+        });
+    };
+
+    // removes a members permissions from a group
+    var removeMember = function() {
+        GroupScheduler.Ajax({
+            url: "Group/RemoveMemberFromGroup",
+            data: { "userId": $(currentRow).find(".hiddenMemberUserId").html(), "groupId": $("#HiddenGroupId").text() },
+            success: function (result) {
+                if (result) {
+                    // member deleted successfully, change the row accordingly
+                    $(currentRow).remove();
+
+                } else {
+                    // error removing member
+                    alert("There was an error removing this member. Please refresh the page and try agian.", "Error");
+                }
+            },
+            error: function () {
+                // error removing memeber
+                alert("There was an error removing this member. Please refresh the page and try agian.", "Error");
+            }
+        });
+    };
+
+    // method to update a group members membership
+    var updateMemberRole = function () {
+        var updatedMember = buildGroupMemberObject();
+        GroupScheduler.Ajax({
+            url: "Group/UpdateMemberRole",
+            data: { "updatedMember": updatedMember, "groupId": $("#HiddenGroupId").text() },
+            success: function (result) {
+                if (result) {
+                    // role updated successfully, change the row accordingly
+                    if (updatedMember.IsAdmin) {
+                        $(currentRow).removeClass("success").addClass("error");
+                        $(currentRow).find(".memberRoleCell").html("Admin");
+                        $(currentRow).find(".DemoteAdmin, .PromoteMember").html("Demote to Member").addClass("btn-danger");
+                    } else {
+                        $(currentRow).removeClass("error").addClass("success");
+                        $(currentRow).find(".memberRoleCell").html("Member");
+                        $(currentRow).find(".DemoteAdmin, .PromoteMember").html("Promote to Admin").addClass("btn-success");
+                    }
+                    
+                } else {
+                    // error updated role
+                    alert("There was an error updating this role. Please refresh the page and try agian.", "Error");
+                }
+            },
+            error: function () {
+                // error updated role
+                alert("There was an error updating this role. Please refresh the page and try agian.", "Error");
+            }
+        });
+    };
+    // method to build the GroupMember object 
+    var buildGroupMemberObject = function() {
+        var groupMember = {};
+        groupMember.UserId = $(currentRow).find(".hiddenMemberUserId").html();
+        groupMember.IsAdmin = $(currentRow).find(".memberRoleCell").html() == "Admin" ? 0 : 1;
+        return groupMember;
+    };
+};
---
+++ GroupScheduler/obj/Release/Package/PackageTmp/scripts/GroupScheduler/Home/Home.js
@@ -0,0 +1,103 @@
+﻿$(document).ready(function () {
+    HomePage.Init();
+});
+
+var HomePage = new function () {
+    // list of groups currently being searched on
+    var globalGroupsList = {};
+    this.Init = function () {
+        
+        // initialize the calendar
+        $('#HomeCalendar').fullCalendar({
+            editable: true,
+            header: {
+                left: 'prev,next today',
+                center: ' title',
+                right: 'month,basicWeek,basicDay'
+            },
+            events: {
+                // grab basic US holidays to throw in the calander
+                url: 'https://www.google.com/calendar/feeds/en.usa%23holiday%40group.v.calendar.google.com/public/basic',
+                className: 'UsHolidays'
+            }
+        });
+
+        // autocomplete initialization for typehead, this is for searching for public groups
+        $("#SearchPublicGroup").typeahead({
+            items: 8,
+            minLength: 3,
+            source: function (query, process) {
+                //return process(getPublicGroups(query));
+                return $.ajax({
+                    url: GroupScheduler.GetLongURL() + "Home/GetAllPublicGroups",
+                    type: 'post',
+                    data: { search: query },
+                    dataType: 'json',
+                    success: function (result) {
+
+                        var resultList = result.map(function (item) {
+                            var aItem = { id: item.id, name: item.name };
+                            return JSON.stringify(aItem);
+                        });
+
+                        return process(resultList);
+
+                    }
+                });
+            },
+            matcher: function (obj) {
+                var item = JSON.parse(obj);
+                return item.name;
+            },
+
+            sorter: function (items) {          
+                var beginswith = [], caseSensitive = [], caseInsensitive = [], item;
+                while (aItem = items.shift()) {
+                    var item = JSON.parse(aItem);
+                    if (!item.name.toLowerCase().indexOf(this.query.toLowerCase())) beginswith.push(JSON.stringify(item));
+                    else if (~item.name.indexOf(this.query)) caseSensitive.push(JSON.stringify(item));
+                    else caseInsensitive.push(JSON.stringify(item));
+                }
+
+                return beginswith.concat(caseSensitive, caseInsensitive);
+
+            },
+
+
+            highlighter: function (obj) {
+                var item = JSON.parse(obj);
+                var query = this.query.replace(/[\-\[\]{}()*+?.,\\\^$|#\s]/g, '\\$&');
+                return item.name.replace(new RegExp('(' + query + ')', 'ig'), function($1, match) {
+                    return '<strong>' + match + '</strong>';
+                });
+            },
+            updater: function (obj) {
+                var item = JSON.parse(obj);
+                window.location.href = GroupScheduler.GetLongURL() + "Group/ViewGroup?groupId=" + item.id;
+                return item.name;
+            }
+        });
+    };
+
+    var getPublicGroups = function (search) {
+        GroupScheduler.Ajax({
+            url: "Home/GetAllPublicGroups",
+            data: { search: search },
+            success: function (result) {
+                globalGroupsList = result;
+                var list = [];
+                for (var i = 0; i < result.length; i++) {
+                    list.push(result[i].name);
+                }
+                return list;
+            },
+            error: function () {
+                globalGroupsList = {};
+                return [];
+            }
+        });
+    };
+
+};
+
+    
